092cc267899a62f3f18971c244235e47b2e485ab
Matt Moyer <moyerm@vmware.com>
2020-07-16 14:24:30 -0500

    Refactor app.go and wire in autoregistration.
    Signed-off-by: Matt Moyer <moyerm@vmware.com>

--- cmd/placeholder-name/app/app_test.go
+++ cmd/placeholder-name/app/app_test.go
@@ -8,45 +8,60 @@ package app
 import (
 	"bytes"
 	"context"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1fake "k8s.io/client-go/kubernetes/fake"
+	aggregationv1fake "k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset/fake"
 )
 
-const knownGoodUsage = `Usage:
+const knownGoodUsage = `
+placeholder-name provides a generic API for mapping an external
+credential from somewhere to an internal credential to be used for
+authenticating to the Kubernetes API.
+
+Usage:
   placeholder-name [flags]
 
 Flags:
-  -c, --config string   path to configuration file (default "placeholder-name.yaml")
-  -h, --help            help for placeholder-name
+  -c, --config string              path to configuration file (default "placeholder-name.yaml")
+      --downward-api-path string   path to Downward API volume mount (default "/etc/podinfo")
+  -h, --help                       help for placeholder-name
 `
 
 func TestCommand(t *testing.T) {
 	tests := []struct {
-		name string
-		args []string
-
-		wantConfigPath string
+		name       string
+		args       []string
+		wantErr    string
+		wantStdout string
 	}{
 		{
-			name:           "NoArgsSucceeds",
-			args:           []string{},
-			wantConfigPath: "placeholder-name.yaml",
+			name: "NoArgsSucceeds",
+			args: []string{},
 		},
 		{
-			name: "OneArgFails",
-			args: []string{"tuna"},
+			name:       "Usage",
+			args:       []string{"-h"},
+			wantStdout: knownGoodUsage,
 		},
 		{
-			name:           "ShortConfigFlagSucceeds",
-			args:           []string{"-c", "some/path/to/config.yaml"},
-			wantConfigPath: "some/path/to/config.yaml",
+			name:    "OneArgFails",
+			args:    []string{"tuna"},
+			wantErr: `unknown command "tuna" for "placeholder-name"`,
 		},
 		{
-			name:           "LongConfigFlagSucceeds",
-			args:           []string{"--config", "some/path/to/config.yaml"},
-			wantConfigPath: "some/path/to/config.yaml",
+			name: "ShortConfigFlagSucceeds",
+			args: []string{"-c", "some/path/to/config.yaml"},
+		},
+		{
+			name: "LongConfigFlagSucceeds",
+			args: []string{"--config", "some/path/to/config.yaml"},
 		},
 		{
 			name: "OneArgWithConfigFlagFails",
@@ -54,32 +69,27 @@ func TestCommand(t *testing.T) {
 				"--config", "some/path/to/config.yaml",
 				"tuna",
 			},
+			wantErr: `unknown command "tuna" for "placeholder-name"`,
 		},
 	}
 	for _, test := range tests {
 		test := test
 		t.Run(test.name, func(t *testing.T) {
-			expect := require.New(t)
-
 			stdout := bytes.NewBuffer([]byte{})
 			stderr := bytes.NewBuffer([]byte{})
 
-			configPaths := make([]string, 0, 1)
-			runFunc := func(ctx context.Context, configPath string) error {
-				configPaths = append(configPaths, configPath)
+			a := New(test.args, stdout, stderr)
+			a.cmd.RunE = func(cmd *cobra.Command, args []string) error {
 				return nil
 			}
-
-			a := New(test.args, stdout, stderr)
-			a.runFunc = runFunc
 			err := a.Run()
-
-			if test.wantConfigPath != "" {
-				expect.Equal(1, len(configPaths))
-				expect.Equal(test.wantConfigPath, configPaths[0])
+			if test.wantErr != "" {
+				require.EqualError(t, err, test.wantErr)
 			} else {
-				expect.Error(err)
-				expect.Contains(stdout.String(), knownGoodUsage)
+				require.NoError(t, err)
+			}
+			if test.wantStdout != "" {
+				require.Equal(t, strings.TrimSpace(test.wantStdout), strings.TrimSpace(stdout.String()))
 			}
 		})
 	}
@@ -88,16 +98,21 @@ func TestCommand(t *testing.T) {
 func TestServeApp(t *testing.T) {
 	t.Parallel()
 
+	fakev1 := corev1fake.NewSimpleClientset(&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "test-namespace"}})
+	fakeaggregationv1 := aggregationv1fake.NewSimpleClientset()
+
 	t.Run("success", func(t *testing.T) {
 		t.Parallel()
 		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
 		cancel()
 
 		a := App{
-			healthAddr: "127.0.0.1:0",
-			mainAddr:   "127.0.0.1:8443",
+			healthAddr:      "127.0.0.1:0",
+			mainAddr:        "127.0.0.1:8443",
+			configPath:      "testdata/valid-config.yaml",
+			downwardAPIPath: "testdata/podinfo",
 		}
-		err := a.serve(ctx, "testdata/valid-config.yaml")
+		err := a.serve(ctx, fakev1.CoreV1(), fakeaggregationv1)
 		require.NoError(t, err)
 	})
 
@@ -107,10 +122,12 @@ func TestServeApp(t *testing.T) {
 		defer cancel()
 
 		a := App{
-			healthAddr: "127.0.0.1:8081",
-			mainAddr:   "127.0.0.1:8081",
+			healthAddr:      "127.0.0.1:8081",
+			mainAddr:        "127.0.0.1:8081",
+			configPath:      "testdata/valid-config.yaml",
+			downwardAPIPath: "testdata/podinfo",
 		}
-		err := a.serve(ctx, "testdata/valid-config.yaml")
+		err := a.serve(ctx, fakev1.CoreV1(), fakeaggregationv1)
 		require.EqualError(t, err, "listen tcp 127.0.0.1:8081: bind: address already in use")
 	})
 }

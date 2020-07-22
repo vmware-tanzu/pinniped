a8c309932d9c80d9e1d3126e59755d6d31d0338f
Andrew Keesler <akeesler@vmware.com>
2020-07-22 11:05:38 -0400

    WIP: integration test passing with real TMC token!

--- test/integration/loginrequest_test.go
+++ test/integration/loginrequest_test.go
@@ -8,6 +8,7 @@ package integration
 import (
 	"context"
 	"encoding/json"
+	"os"
 	"testing"
 	"time"
 
@@ -35,14 +36,17 @@ func makeRequest(t *testing.T, spec v1alpha1.LoginRequestSpec) (*v1alpha1.LoginR
 }
 
 func TestSuccessfulLoginRequest(t *testing.T) {
+	tmcClusterToken := os.Getenv("PLACEHOLDER_NAME_TMC_CLUSTER_TOKEN")
+	require.NotEmptyf(t, tmcClusterToken, "must specify PLACEHOLDER_NAME_TMC_CLUSTER_TOKEN env var for integration tests")
+
 	response, err := makeRequest(t, v1alpha1.LoginRequestSpec{
-		Token: &v1alpha1.LoginRequestTokenCredential{Value: "token-value"},
+		Token: &v1alpha1.LoginRequestTokenCredential{Value: tmcClusterToken},
 	})
 
 	require.NoError(t, err)
 
 	require.Empty(t, response.Spec)
-	require.Equal(t, "snorlax", response.Status.Token)
+	require.NotEmpty(t, response.Status.Token)
 	require.Empty(t, response.Status.ClientCertificateData)
 	require.Empty(t, response.Status.ClientKeyData)
 	require.Nil(t, response.Status.ExpirationTimestamp)
@@ -78,7 +82,7 @@ func TestGetDiscovery(t *testing.T) {
 	require.Contains(t, parsedResult["paths"], "/apis/placeholder.suzerain-io.github.io/v1alpha1")
 }
 
-func TestGetApiResourceList(t *testing.T) {
+func TestGetAPIResourceList(t *testing.T) {
 	var expectedAPIResourceList = `{
 	  "kind": "APIResourceList",
 	  "apiVersion": "v1",
@@ -104,4 +108,76 @@ func TestGetApiResourceList(t *testing.T) {
 	result, err := client.PlaceholderV1alpha1().RESTClient().Get().Do(ctx).Raw()
 	require.NoError(t, err)
 	require.JSONEq(t, expectedAPIResourceList, string(result))
+
+	// proposed:
+
+	groups, resources, err := client.Discovery().ServerGroupsAndResources()
+	require.NoError(t, err)
+
+	groupName := "placeholder.suzerain-io.github.io"
+	actualGroup := findGroup(groupName, groups)
+	require.NotNil(t, actualGroup)
+
+	expectedGroup := &metav1.APIGroup{
+		Name: "placeholder.suzerain-io.github.io",
+		Versions: []metav1.GroupVersionForDiscovery{
+			metav1.GroupVersionForDiscovery{
+				GroupVersion: "placeholder.suzerain-io.github.io/v1alpha1",
+				Version:      "v1alpha1",
+			},
+		},
+		PreferredVersion: metav1.GroupVersionForDiscovery{
+			GroupVersion: "placeholder.suzerain-io.github.io/v1alpha1",
+			Version:      "v1alpha1",
+		},
+	}
+	require.Equal(t, expectedGroup, actualGroup)
+
+	resourceGroupVersion := "placeholder.suzerain-io.github.io/v1alpha1"
+	actualResources := findResources(resourceGroupVersion, resources)
+	require.NotNil(t, actualResources)
+
+	expectedResources := &metav1.APIResourceList{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "APIResourceList",
+			APIVersion: "v1",
+		},
+		GroupVersion: "placeholder.suzerain-io.github.io/v1alpha1",
+		APIResources: []metav1.APIResource{
+			metav1.APIResource{
+				Name: "loginrequests",
+				Kind: "LoginRequest",
+				Verbs: metav1.Verbs([]string{
+					"create",
+				}),
+			},
+		},
+	}
+	require.Equal(t, expectedResources, actualResources)
+}
+
+func TestGetAPIVersion(t *testing.T) {
+	client := library.NewPlaceholderNameClientset(t)
+
+	version, err := client.Discovery().ServerVersion()
+	require.NoError(t, err)
+	require.NotNil(t, version) // TODO(akeesler: what can we assert here?
+}
+
+func findGroup(name string, groups []*metav1.APIGroup) *metav1.APIGroup {
+	for _, group := range groups {
+		if group.Name == name {
+			return group
+		}
+	}
+	return nil
+}
+
+func findResources(groupVersion string, resources []*metav1.APIResourceList) *metav1.APIResourceList {
+	for _, resource := range resources {
+		if resource.GroupVersion == groupVersion {
+			return resource
+		}
+	}
+	return nil
 }

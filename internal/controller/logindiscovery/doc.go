ee865fe97f44b9d98a22a8cdd61d56992b3de721
Andrew Keesler <akeesler@vmware.com>
2020-07-30 10:39:38 -0400

    logindiscovery: add package documentation.
    Signed-off-by: Andrew Keesler <akeesler@vmware.com>

---
+++ internal/controller/logindiscovery/doc.go
@@ -0,0 +1,7 @@
+/*
+Copyright 2020 VMware, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+// Package logindiscovery contains controller(s) for reconciling LoginDiscoveryConfig's.
+package logindiscovery

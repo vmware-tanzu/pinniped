4cb0fd394987cd167e436a5c1f42c667a336e6c0
Ryan Richard <richardry@vmware.com>
2020-08-11 17:55:34 -0700

    Use a DaemonSet instead of a Deployment to deploy our app
    - For high availability reasons, we would like our app to scale linearly
      with the size of the control plane. Using a DaemonSet allows us to run
      one pod on each node-role.kubernetes.io/master node.
    - The hope is that the Service that we create should load balance
      between these pods appropriately.

--- test/integration/app_availability_test.go
+++ test/integration/app_availability_test.go
@@ -11,27 +11,23 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/suzerain-io/placeholder-name/test/library"
 )
 
-func TestGetDeployment(t *testing.T) {
+func TestAppAvailability(t *testing.T) {
 	library.SkipUnlessIntegration(t)
 	namespaceName := library.Getenv(t, "PLACEHOLDER_NAME_NAMESPACE")
-	deploymentName := library.Getenv(t, "PLACEHOLDER_NAME_DEPLOYMENT")
+	daemonSetName := library.Getenv(t, "PLACEHOLDER_NAME_APP_NAME")
 
 	client := library.NewClientset(t)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	appDeployment, err := client.AppsV1().Deployments(namespaceName).Get(ctx, deploymentName, metav1.GetOptions{})
+	daemonSet, err := client.AppsV1().DaemonSets(namespaceName).Get(ctx, daemonSetName, metav1.GetOptions{})
 	require.NoError(t, err)
 
-	cond := library.GetDeploymentCondition(appDeployment.Status, appsv1.DeploymentAvailable)
-	require.NotNil(t, cond)
-	require.Equalf(t, corev1.ConditionTrue, cond.Status, "app should be available: %s", library.Sdump(appDeployment))
+	require.GreaterOrEqual(t, daemonSet.Status.NumberAvailable, int32(1))
 }

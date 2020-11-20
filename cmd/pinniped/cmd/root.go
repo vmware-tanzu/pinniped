53bece218621347df69b595ff9f45ae457874ec8
Rajat Goyal <404rajat@gmail.com>
2020-11-20 23:49:52 +0530

    Avoid printing the error message twice from client

--- cmd/pinniped/cmd/root.go
+++ cmd/pinniped/cmd/root.go
@@ -4,7 +4,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -22,7 +21,6 @@ var rootCmd = &cobra.Command{
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }

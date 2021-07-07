f1e63c55d4859b5f7cc53ac8161cb5aede284094
Ryan Richard <richardry@vmware.com>
2021-07-07 12:50:13 -0700

    Add `https_proxy` and `no_proxy` settings for the Supervisor
    - Add new optional ytt params for the Supervisor deployment.
    - When the Supervisor is making calls to an upstream OIDC provider,
      use these variables if they were provided.
    - These settings are integration tested in the main CI pipeline by
      sometimes setting them on deployments in certain cases, and then
      letting the existing integration tests (e.g. TestE2EFullIntegration)
      provide the coverage, so there are no explicit changes to the
      integration tests themselves in this commit.

--- internal/controller/supervisorconfig/oidcupstreamwatcher/oidc_upstream_watcher.go
+++ internal/controller/supervisorconfig/oidcupstreamwatcher/oidc_upstream_watcher.go
@@ -263,9 +263,13 @@ func (c *oidcWatcherController) validateIssuer(ctx context.Context, upstream *v1
 				Message: err.Error(),
 			}
 		}
-		httpClient = &http.Client{Transport: &http.Transport{TLSClientConfig: tlsConfig}}
 
-		discoveredProvider, err = oidc.NewProvider(oidc.ClientContext(ctx, httpClient), upstream.Spec.Issuer)
+		httpClient = &http.Client{Transport: &http.Transport{Proxy: http.ProxyFromEnvironment, TLSClientConfig: tlsConfig}}
+
+		timeoutCtx, cancelFunc := context.WithTimeout(oidc.ClientContext(ctx, httpClient), time.Minute)
+		defer cancelFunc()
+
+		discoveredProvider, err = oidc.NewProvider(timeoutCtx, upstream.Spec.Issuer)
 		if err != nil {
 			const klogLevelTrace = 6
 			c.log.V(klogLevelTrace).WithValues(

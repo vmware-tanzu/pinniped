1388183bf1f6e34b9ecb0d1861168460ec373ad9
Mo Khan <i@monis.app>
2022-02-07 20:53:03 -0500

    TestE2EFullIntegration: reduce timeout
    This causes the test to timeout before concourse terminates the entire test run.

--- test/integration/e2e_test.go
+++ test/integration/e2e_test.go
@@ -50,7 +50,7 @@ import (
 func TestE2EFullIntegration(t *testing.T) {
 	env := testlib.IntegrationEnv(t)
 
-	ctx, cancelFunc := context.WithTimeout(context.Background(), 15*time.Minute)
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancelFunc()
 
 	// Build pinniped CLI.

d374b468d8a67e0f6bc19e014a3e6efe5e03a978
Margo Crawford <margaretc@vmware.com>
2022-03-15 16:56:29 -0700

    Using different cipher suites for fips
    Signed-off-by: Monis Khan <mok@vmware.com>

--- internal/crypto/ptls/default.go
+++ internal/crypto/ptls/default.go
@@ -1,34 +1,15 @@
-// Copyright 2022 the Pinniped contributors. All Rights Reserved.
+// Copyright 2021-2022 the Pinniped contributors. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-//go:build fips_strict
-// +build fips_strict
+//go:build !fips_strict
+// +build !fips_strict
 
 package ptls
 
 import (
 	"crypto/tls"
-	_ "crypto/tls/fipsonly" // restricts all TLS configuration to FIPS-approved settings.
 	"crypto/x509"
-	"log"
-	"time"
 )
-
-func init() {
-	go func() {
-		time.Sleep(5 * time.Second)
-		log.Println("using boringcrypto in fips only mode.")
-	}()
-}
-
-// FIPS does not support TLS 1.3.
-// Therefore, we cannot use Pinniped's usual secure configuration,
-// which requires TLS 1.3.
-// We also have a shorter list of 1.2 suites to choose from.
-// Secure is just a wrapper for Default in this case.
-func Secure(rootCAs *x509.CertPool) *tls.Config {
-	return Default(rootCAs)
-}
 
 func Default(rootCAs *x509.CertPool) *tls.Config {
 	return &tls.Config{
@@ -62,9 +43,9 @@ func Default(rootCAs *x509.CertPool) *tls.Config {
 		CipherSuites: []uint16{
 			// these are all AEADs with ECDHE, some use ChaCha20Poly1305 while others use AES-GCM
 			// this provides forward secrecy, confidentiality and authenticity of data
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256, tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384, tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 		},
 
 		// enable HTTP2 for go's 1.7 HTTP Server
---
+++ internal/crypto/ptls/secure.go
@@ -0,0 +1,35 @@
+// Copyright 2021-2022 the Pinniped contributors. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build !fips_strict
+// +build !fips_strict
+
+package ptls
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+)
+
+func Secure(rootCAs *x509.CertPool) *tls.Config {
+	// as of 2021-10-19, Mozilla Guideline v5.6, Go 1.17.2, modern configuration, supports:
+	// - Firefox 63
+	// - Android 10.0
+	// - Chrome 70
+	// - Edge 75
+	// - Java 11
+	// - OpenSSL 1.1.1
+	// - Opera 57
+	// - Safari 12.1
+	// https://ssl-config.mozilla.org/#server=go&version=1.17.2&config=modern&guideline=5.6
+	c := Default(rootCAs)
+	c.MinVersion = tls.VersionTLS13 // max out the security
+	c.CipherSuites = []uint16{
+		// TLS 1.3 ciphers are not configurable, but we need to explicitly set them here to make our client hello behave correctly
+		// See https://github.com/golang/go/pull/49293
+		tls.TLS_AES_128_GCM_SHA256,
+		tls.TLS_AES_256_GCM_SHA384,
+		tls.TLS_CHACHA20_POLY1305_SHA256,
+	}
+	return c
+}

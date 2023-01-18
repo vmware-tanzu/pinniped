3b46547efc374a3e40a4ec13ae51abb22a1772bb
Ryan Richard <richardry@vmware.com>
2023-01-18 13:36:23 -0800

    add hack/update-copyright-year.sh

--- hack/check-copyright-year.sh
+++ hack/check-copyright-year.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-# Copyright 2021 the Pinniped contributors. All Rights Reserved.
+# Copyright 2021-2023 the Pinniped contributors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-#
-# Check if copyright statements include the current year
-#
+
+set -euo pipefail
+
 files=$(git diff --cached --name-only)
 year=$(date +"%Y")
 
-for f in $files; do
-    head -10 $f | grep -i copyright 2>&1 1>/dev/null || continue
+missing_copyright_files=()
 
-    if ! grep -i -e "copyright.*$year" $f 2>&1 1>/dev/null; then
-        missing_copyright_files="$missing_copyright_files $f"
+for f in $files; do
+    head -10 "$f" | grep -i 'Copyright.*the Pinniped contributors' 2>&1 1>/dev/null || continue
+
+    if ! head -10 "$f" | grep -i -e "Copyright.*$year.*the Pinniped contributors" 2>&1 1>/dev/null; then
+        missing_copyright_files+=("$f")
     fi
 done
 
-if [ -n "$missing_copyright_files" ]; then
+if [[ "${#missing_copyright_files[@]}" -gt "0" ]]; then
     echo "Copyright notice should include the year the file was created and the year the file was last modified."
     echo "$year is missing in the copyright notice of the following files:"
-    for f in $missing_copyright_files; do
+    for f in "${missing_copyright_files[@]}"; do
         echo "    $f"
     done
+    echo "Try using hack/update-copyright-year.sh to update the copyright automatically in staged files."
     exit 1
 fi

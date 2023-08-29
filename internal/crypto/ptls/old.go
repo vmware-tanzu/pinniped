05a1187e2ef8d4f961dc60919237e2ced8b7baf8
Joshua Casey <joshuatcasey@gmail.com>
2023-08-29 16:59:24 -0500

    Simplify build tags associated with unsupported golang versions

--- internal/crypto/ptls/old.go
+++ internal/crypto/ptls/old.go
@@ -1,8 +1,8 @@
-// Copyright 2021 the Pinniped contributors. All Rights Reserved.
+// Copyright 2021-2023 the Pinniped contributors. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-//go:build !go1.17
-// +build !go1.17
+//go:build !go1.19
+// +build !go1.19
 
 package ptls
 
@@ -10,6 +10,6 @@ func init() {
 	// cause compile time failure if an older version of Go is used
 	`Pinniped's TLS configuration makes assumptions about how the Go standard library implementation of TLS works.
 It particular, we rely on the server controlling cipher suite selection.  For these assumptions to hold, Pinniped
-must be compiled with Go 1.17+.  If you are seeing this error message, your attempt to compile Pinniped with an
+must be compiled with Go 1.19+.  If you are seeing this error message, your attempt to compile Pinniped with an
 older Go compiler was explicitly failed to prevent an unsafe configuration.`
 }

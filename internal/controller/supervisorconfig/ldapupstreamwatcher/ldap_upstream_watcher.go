fd14a5794ef65b58449347382c386f2db9230831
Benjamin A. Petersen <ben@benjaminapetersen.me>
2024-02-12 12:38:57 -0500

    ldap upstream watcher: rename local var for clarity

--- internal/controller/supervisorconfig/ldapupstreamwatcher/ldap_upstream_watcher.go
+++ internal/controller/supervisorconfig/ldapupstreamwatcher/ldap_upstream_watcher.go
@@ -209,9 +209,9 @@ func (c *ldapWatcherController) Sync(ctx controllerlib.Context) error {
 	requeue := false
 	validatedUpstreams := make([]upstreamprovider.UpstreamLDAPIdentityProviderI, 0, len(actualUpstreams))
 	for _, upstream := range actualUpstreams {
-		valid, requestedRequeue := c.validateUpstream(ctx.Context, upstream)
-		if valid != nil {
-			validatedUpstreams = append(validatedUpstreams, valid)
+		validProvider, requestedRequeue := c.validateUpstream(ctx.Context, upstream)
+		if validProvider != nil {
+			validatedUpstreams = append(validatedUpstreams, validProvider)
 		}
 		if requestedRequeue {
 			requeue = true

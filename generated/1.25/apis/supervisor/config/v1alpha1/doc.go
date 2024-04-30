9d21bcacb6f762396732ec3f9a83f7b366b31c98
Ryan Richard <richardry@vmware.com>
2024-04-30 14:06:40 -0700

    rerun codegen: adds rest of 1.30 codegen, updates old generated doc files

--- generated/1.25/apis/supervisor/config/v1alpha1/doc.go
+++ generated/1.25/apis/supervisor/config/v1alpha1/doc.go
@@ -1,10 +1,7 @@
-// Copyright 2020-2022 the Pinniped contributors. All Rights Reserved.
+// Copyright 2020-2024 the Pinniped contributors. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// +k8s:openapi-gen=true
 // +k8s:deepcopy-gen=package
-// +k8s:conversion-gen=go.pinniped.dev/generated/1.25/apis/supervisor/config
-// +k8s:defaulter-gen=TypeMeta
 // +groupName=config.supervisor.pinniped.dev
 
 // Package v1alpha1 is the v1alpha1 version of the Pinniped supervisor configuration API.
--- generated/1.30/apis/go.mod
+++ generated/1.30/apis/go.mod
@@ -3,6 +3,8 @@ module go.pinniped.dev/generated/1.30/apis
 
 go 1.22.0
 
+toolchain go1.22.2
+
 require (
 	k8s.io/api v0.30.0
 	k8s.io/apimachinery v0.30.0

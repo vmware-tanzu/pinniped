108bb923b513b093706688d5c6e599b9f36a7b28
Ryan Richard <richardry@vmware.com>
2024-07-22 15:41:46 -0700

    run codegen to update copyrights

--- generated/1.29/apis/concierge/authentication/v1alpha1/types_tls.go
+++ generated/1.29/apis/concierge/authentication/v1alpha1/types_tls.go
@@ -1,4 +1,4 @@
-// Copyright 2020-2022 the Pinniped contributors. All Rights Reserved.
+// Copyright 2020-2024 the Pinniped contributors. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 package v1alpha1

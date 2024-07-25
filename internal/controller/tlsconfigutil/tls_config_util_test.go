30d82e1a179b3f510d661dd972eab03ed865543d
Joshua Casey <joshuatcasey@gmail.com>
2024-07-25 14:19:17 -0500

    When reading CA bundle from a secret/configmap, return more specific err
    When the bundle does not contain any certs, make the error more
    specific.
    
    Co-authored-by: Ryan Richard <richardry@vmware.com>

--- internal/controller/tlsconfigutil/tls_config_util_test.go
+++ internal/controller/tlsconfigutil/tls_config_util_test.go
@@ -84,7 +84,7 @@ func TestValidateTLSConfig(t *testing.T) {
 				Type:    typeTLSConfigurationValid,
 				Status:  metav1.ConditionFalse,
 				Reason:  ReasonInvalidTLSConfig,
-				Message: "spec.foo.tls.certificateAuthorityData is invalid: no certificates found",
+				Message: `spec.foo.tls.certificateAuthorityData is invalid: no base64-encoded PEM certificates found in 88 bytes of data (PEM certificates must begin with "-----BEGIN CERTIFICATE-----")`,
 			},
 		},
 		{
@@ -266,6 +266,35 @@ func TestValidateTLSConfig(t *testing.T) {
 			},
 		},
 		{
+			name: "should return invalid condition when a secret has the configured key but the value is not a cert",
+			tlsSpec: &TLSSpec{
+				CertificateAuthorityDataSource: &caBundleSource{
+					Kind: "Secret",
+					Name: "awesome-secret",
+					Key:  "ca-bundle",
+				},
+			},
+			namespace: "awesome-namespace",
+			k8sObjects: []runtime.Object{
+				&corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "awesome-secret",
+						Namespace: "awesome-namespace",
+					},
+					Type: corev1.SecretTypeOpaque,
+					Data: map[string][]byte{
+						"ca-bundle": []byte("this is not a certificate"),
+					},
+				},
+			},
+			expectedCondition: &metav1.Condition{
+				Type:    typeTLSConfigurationValid,
+				Status:  metav1.ConditionFalse,
+				Reason:  ReasonInvalidTLSConfig,
+				Message: `spec.foo.tls.certificateAuthorityDataSource is invalid: key "ca-bundle" with 25 bytes of data in secret "awesome-namespace/awesome-secret" is not a PEM-encoded certificate (PEM certificates must begin with "-----BEGIN CERTIFICATE-----")`,
+			},
+		},
+		{
 			name: "should return invalid condition when a configmap does not have the configured key",
 			tlsSpec: &TLSSpec{
 				CertificateAuthorityDataSource: &caBundleSource{
@@ -319,6 +348,34 @@ func TestValidateTLSConfig(t *testing.T) {
 				Status:  metav1.ConditionFalse,
 				Reason:  ReasonInvalidTLSConfig,
 				Message: `spec.foo.tls.certificateAuthorityDataSource is invalid: key "ca-bundle" has empty value in configmap "awesome-namespace/awesome-configmap"`,
+			},
+		},
+		{
+			name: "should return invalid condition when a configmap has the configured key but its value not a cert",
+			tlsSpec: &TLSSpec{
+				CertificateAuthorityDataSource: &caBundleSource{
+					Kind: "ConfigMap",
+					Name: "awesome-configmap",
+					Key:  "ca-bundle",
+				},
+			},
+			namespace: "awesome-namespace",
+			k8sObjects: []runtime.Object{
+				&corev1.ConfigMap{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "awesome-configmap",
+						Namespace: "awesome-namespace",
+					},
+					Data: map[string]string{
+						"ca-bundle": "this is not a cert",
+					},
+				},
+			},
+			expectedCondition: &metav1.Condition{
+				Type:    typeTLSConfigurationValid,
+				Status:  metav1.ConditionFalse,
+				Reason:  ReasonInvalidTLSConfig,
+				Message: `spec.foo.tls.certificateAuthorityDataSource is invalid: key "ca-bundle" with 18 bytes of data in configmap "awesome-namespace/awesome-configmap" is not a PEM-encoded certificate (PEM certificates must begin with "-----BEGIN CERTIFICATE-----")`,
 			},
 		},
 		{

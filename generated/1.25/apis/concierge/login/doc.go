458bc8a752144bde4e78841a9f9e8d5c40078bde
Joshua Casey <joshua.casey@broadcom.com>
2024-12-16 11:22:20 -0600

    Drop codegen for 0.25

--- generated/1.25/apis/concierge/login/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2020-2022 the Pinniped contributors. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// +k8s:deepcopy-gen=package
-// +groupName=login.concierge.pinniped.dev
-
-// Package login is the internal version of the Pinniped login API.
-package login

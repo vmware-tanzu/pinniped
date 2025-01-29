6cd832bd56d556b93d969cc2328200f4e423e049
Joshua Casey <joshua.casey@broadcom.com>
2025-01-29 13:52:35 -0600

    Bump libs to k8s.io@v0.32.2, add codegen for k8s 1.32, and drop codegen for k8s 1.25

--- generated/latest/client/supervisor/listers/config/v1alpha1/oidcclient.go
+++ generated/latest/client/supervisor/listers/config/v1alpha1/oidcclient.go
@@ -6,10 +6,10 @@
 package v1alpha1
 
 import (
-	v1alpha1 "go.pinniped.dev/generated/latest/apis/supervisor/config/v1alpha1"
-	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/client-go/listers"
-	"k8s.io/client-go/tools/cache"
+	configv1alpha1 "go.pinniped.dev/generated/latest/apis/supervisor/config/v1alpha1"
+	labels "k8s.io/apimachinery/pkg/labels"
+	listers "k8s.io/client-go/listers"
+	cache "k8s.io/client-go/tools/cache"
 )
 
 // OIDCClientLister helps list OIDCClients.
@@ -17,7 +17,7 @@ import (
 type OIDCClientLister interface {
 	// List lists all OIDCClients in the indexer.
 	// Objects returned here must be treated as read-only.
-	List(selector labels.Selector) (ret []*v1alpha1.OIDCClient, err error)
+	List(selector labels.Selector) (ret []*configv1alpha1.OIDCClient, err error)
 	// OIDCClients returns an object that can list and get OIDCClients.
 	OIDCClients(namespace string) OIDCClientNamespaceLister
 	OIDCClientListerExpansion
@@ -25,17 +25,17 @@ type OIDCClientLister interface {
 
 // oIDCClientLister implements the OIDCClientLister interface.
 type oIDCClientLister struct {
-	listers.ResourceIndexer[*v1alpha1.OIDCClient]
+	listers.ResourceIndexer[*configv1alpha1.OIDCClient]
 }
 
 // NewOIDCClientLister returns a new OIDCClientLister.
 func NewOIDCClientLister(indexer cache.Indexer) OIDCClientLister {
-	return &oIDCClientLister{listers.New[*v1alpha1.OIDCClient](indexer, v1alpha1.Resource("oidcclient"))}
+	return &oIDCClientLister{listers.New[*configv1alpha1.OIDCClient](indexer, configv1alpha1.Resource("oidcclient"))}
 }
 
 // OIDCClients returns an object that can list and get OIDCClients.
 func (s *oIDCClientLister) OIDCClients(namespace string) OIDCClientNamespaceLister {
-	return oIDCClientNamespaceLister{listers.NewNamespaced[*v1alpha1.OIDCClient](s.ResourceIndexer, namespace)}
+	return oIDCClientNamespaceLister{listers.NewNamespaced[*configv1alpha1.OIDCClient](s.ResourceIndexer, namespace)}
 }
 
 // OIDCClientNamespaceLister helps list and get OIDCClients.
@@ -43,15 +43,15 @@ func (s *oIDCClientLister) OIDCClients(namespace string) OIDCClientNamespaceList
 type OIDCClientNamespaceLister interface {
 	// List lists all OIDCClients in the indexer for a given namespace.
 	// Objects returned here must be treated as read-only.
-	List(selector labels.Selector) (ret []*v1alpha1.OIDCClient, err error)
+	List(selector labels.Selector) (ret []*configv1alpha1.OIDCClient, err error)
 	// Get retrieves the OIDCClient from the indexer for a given namespace and name.
 	// Objects returned here must be treated as read-only.
-	Get(name string) (*v1alpha1.OIDCClient, error)
+	Get(name string) (*configv1alpha1.OIDCClient, error)
 	OIDCClientNamespaceListerExpansion
 }
 
 // oIDCClientNamespaceLister implements the OIDCClientNamespaceLister
 // interface.
 type oIDCClientNamespaceLister struct {
-	listers.ResourceIndexer[*v1alpha1.OIDCClient]
+	listers.ResourceIndexer[*configv1alpha1.OIDCClient]
 }
